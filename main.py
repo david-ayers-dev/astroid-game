66131b2a31d12755839a162c65b808f5880b5382
ZomaTheLegend <cggeek1@gmail.com>
2026-03-03 17:56:10 -0600

    2: Ch2L4

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     while True:
         log_state()
 
@@ -23,6 +27,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+  
+        dt = clock.tick(60) / 1000
+
     
 if __name__ == "__main__":
     main()
